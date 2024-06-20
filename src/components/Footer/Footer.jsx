678e0b2f20e05e6206603c7321c1ff4d31c30284
annaeckman <annaeckman@icloud.com>
2024-06-20 12:12:43 -0700

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "../Footer/footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__author">developed by anna eckman</p>
+      <p className="footer__date">2024</p>
+    </div>
+  );
+}
+
+export default Footer;
