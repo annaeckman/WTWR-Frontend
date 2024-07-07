d56ed58da741c086e341ced684f57d652e38300b
annaeckman <annaeckman@icloud.com>
2024-07-07 10:28:08 -0700

    added utils.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,18 +1,15 @@
+import { processServerResponse } from "../utils/utils";
 const baseUrl = "http://localhost:3001";
 
 function getItems() {
-  return fetch(`${baseUrl}/items`).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
+  return fetch(`${baseUrl}/items`).then(processServerResponse);
 }
 
 function deleteItem(id) {
   return fetch(`${baseUrl}/items/${id}`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
-  }).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
+  }).then(processServerResponse);
 }
 
 function addItem({ name, weather, imageUrl }) {
@@ -20,9 +17,7 @@ function addItem({ name, weather, imageUrl }) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, weather, imageUrl }),
-  }).then((res) => {
-    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
-  });
+  }).then(processServerResponse);
 }
 
 export { getItems, deleteItem, addItem };
